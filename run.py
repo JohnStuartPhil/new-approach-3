1b1997c56499b56f88a876bab67a047159f5af0e
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-30 14:20:03 +0000

    Add dictionary

--- run.py
+++ run.py
@@ -1,3 +1,69 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def the_options():
+    """data"""
+    options = [
+        {
+            'country': 'the United States',
+            'capital': 'Washington DC',
+            'incorrect_1': 'Los Angeles',
+            'incorrect_2': 'New York'
+        },
+        {
+            'country': 'China',
+            'capital': 'Beijing',
+            'incorrect_1': 'Hong Kong',
+            'incorrect_2': 'Shanghai'
+        },
+        {
+            'country': 'Germany',
+            'capital': 'Berlin',
+            'incorrect_1': 'Hamburg',
+            'incorrect_2': 'Munich'
+        },
+        {
+            'country': 'Japan',
+            'capital': 'Tokyo',
+            'incorrect_1': 'Hiroshima',
+            'incorrect_2': 'Osaka'
+        },
+        {
+            'country': 'India',
+            'capital': 'New Dheli',
+            'incorrect_1': 'Chennai',
+            'incorrect_2': 'Mumbai'
+        },
+        {
+            'country': 'the United Kingdom',
+            'capital': 'London',
+            'incorrect_1': 'Birmingham',
+            'incorrect_2': 'Manchester'
+        },
+        {
+            'country': 'France',
+            'capital': 'Paris',
+            'incorrect_1': 'Lyon',
+            'incorrect_2': 'Nice'
+        },
+        {
+            'country': 'Italy',
+            'capital': 'Rome',
+            'incorrect_1': 'Milan',
+            'incorrect_2': 'Naples'
+        },
+        {
+            'country': 'Canada',
+            'capital': 'Ottawa',
+            'incorrect_1': 'Montreal',
+            'incorrect_2': 'Toronto'
+        },
+        {
+            'country': 'Brazil',
+            'capital': 'Brasilia',
+            'incorrect_1': 'Rio de Janerio',
+            'incorrect_2': 'Sao Paulo'
+        }
+    ]
+
+
+    
