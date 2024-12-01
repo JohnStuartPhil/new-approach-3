87f0129fd9397ddb8dbbb2b49170199ff3be0b6b
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-12-01 14:49:58 +0000

    Add spaces when advising of correct option or not

--- run.py
+++ run.py
@@ -120,17 +120,17 @@ def the_quiz():
                 break
 
         if available_options.upper() == 'A' and options[0] == question.capital:
-            console.print("[bold]Well done, that is the correct option", style="green")
+            console.print("[bold]\nWell done, that is the correct option", style="green")
             score += 1
-            print("Your score is currently:", score)
+            print("\nYour score is currently:", score)
         elif available_options.upper() == 'B' and options[1] == question.capital:
-            console.print("[bold]Well done, that is the correct option", style="green")
+            console.print("[bold]\nWell done, that is the correct option", style="green")
             score += 1
-            print("Your score is currently:", score)
+            print("\nYour score is currently:", score)
         elif available_options.upper() == 'C' and options[2] == question.capital:
-            console.print("[bold]Well done, that is the correct option", style="green")
+            console.print("[bold]\nWell done, that is the correct option", style="green")
             score += 1
-            print("Your score is currently:", score)
+            print("\nYour score is currently:", score)
         else:
             console.print("[bold]\nSorry, that was not the correct option", style="red")
             print("\nYour score remains at:", score)
